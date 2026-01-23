eff011ac64e5cf7b698f6d41b834e83c075709a4
sk <sk@alien>
2026-01-23 03:58:42 +0300

    refactor: root devenv

--- devenv.nix
+++ devenv.nix
@@ -1,7 +1,21 @@
-{ ... }:
-{
-  env.GREET = "devenvs";
+{ config, lib, pkgs, ... }:
+let
+  inherit (lib.lists) map;
 
+  imports-args = {
+    inherit config lib pkgs;
+  };
+in
+{
+  imports = map (modulePath: import modulePath imports-args) [
+    ./modules/json.nix
+    ./modules/toml.nix
+  ];
+
+  knopki.json.enable = true;
+  knopki.toml.enable = true;
+
+  env.GREET = "devenvs";
 
   treefmt.enable = true;
 }
--- nixos/devenv.nix
+++ nixos/devenv.nix
@@ -1,20 +1,20 @@
-args@{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
   inherit (lib.modules) mkDefault;
   inherit (lib.lists) map;
 
-  imports-args = args // {
-    # inherit recipes-lib;
-    # inherit pkgs-unstable;
+  imports-args = {
+    inherit config lib pkgs;
   };
 in
 {
-
   imports = map (modulePath: import modulePath imports-args) [
     ../modules/git.nix
     ../modules/json.nix
+    ../modules/toml.nix
   ];
 
   knopki.git.enable = mkDefault true;
   knopki.json.enable = mkDefault true;
+  knopki.toml.enable = mkDefault true;
 }

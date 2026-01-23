4866e9c5a498103095b9817f0df5690c789ae0cd
sk <sk@alien>
2026-01-23 03:43:30 +0300

    feat: json module

---
+++ modules/json.nix
@@ -0,0 +1,42 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:let
+  inherit (lib.modules) mkDefault mkIf;
+  inherit (lib.options) mkEnableOption mkPackageOption;
+  inherit (lib.lists) optional;
+
+  cfg = config.knopki.json;
+in
+{
+  options.knopki.json = {
+    enable = mkEnableOption "Enable json support";
+
+    jq = {
+      enable = mkEnableOption "Enable jq" // { default = true; };
+      package = mkPackageOption pkgs "jq" {};
+    };
+
+    fx = {
+      enable = mkEnableOption "Enable fx" // { default = true; };
+      package = mkPackageOption pkgs "fx" {};
+    };
+  };
+
+  config = mkIf cfg.enable {
+    packages = optional cfg.jq.enable cfg.jq.package ++ optional cfg.fx.enable cfg.fx.package;
+
+
+    git-hooks.hooks = {
+      check-json.enable = mkDefault true;
+      denofmt.enable = mkDefault true;
+    };
+
+    treefmt.config.programs = {
+      deno.enable = mkDefault true;
+      formatjson5.enable = mkDefault true;
+    };
+  };
+}
--- nixos/devenv.nix
+++ nixos/devenv.nix
@@ -1,25 +1,20 @@
-args@{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+args@{ lib, pkgs, ... }:
 let
-  inherit (lib.modules) mkIf mkDefault;
+  inherit (lib.modules) mkDefault;
   inherit (lib.lists) map;
-  inherit (lib.types) bool;
-  inherit (lib.options) mkOption;
 
   imports-args = args // {
     # inherit recipes-lib;
     # inherit pkgs-unstable;
   };
-
-  cfg = config.knopki;
 in
 {
 
-  imports = map (modulePath: import modulePath imports-args) [ ../modules/git.nix ];
+  imports = map (modulePath: import modulePath imports-args) [
+    ../modules/git.nix
+    ../modules/json.nix
+  ];
 
   knopki.git.enable = mkDefault true;
+  knopki.json.enable = mkDefault true;
 }

371702b5c6fe91c4923af3b89b5c3909c1c86a1a
Red Buttons <knopki@duck.com>
2026-01-26 21:51:53 +0300

    feat(containers): add lazydocker

--- modules/containers.nix
+++ modules/containers.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 let
@@ -20,10 +21,19 @@ in
       };
       package = mkPackageOption config.git-hooks.hooks.hadolint "package" { };
     };
+
+    lazydocker = {
+      enable = mkEnableOption "Enable lazydocker" // {
+        default = true;
+      };
+      package = mkPackageOption pkgs "lazydocker" { };
+    };
   };
 
   config = mkIf cfg.enable {
-    packages = optional cfg.hadolint.enable cfg.hadolint.package;
+    packages =
+      optional cfg.hadolint.enable cfg.hadolint.package
+      ++ optional cfg.lazydocker.enable cfg.lazydocker.package;
 
     git-hooks.hooks = {
       hadolint.enable = mkDefault cfg.hadolint.enable;
@@ -32,6 +42,9 @@ in
     knopki.menu.commands = map (cmd: cmd // { category = "containers"; }) (
       optional cfg.hadolint.enable {
         inherit (cfg.hadolint) package;
+      }
+      ++ optional cfg.lazydocker.enable {
+        inherit (cfg.lazydocker) package;
       }
     );
   };
